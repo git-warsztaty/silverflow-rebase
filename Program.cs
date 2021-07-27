39feb350b826b40f59cf42cf64d468eb44ff67be
Tomasz Skraskowski <tomasz.skraskowski@pirios.com>
2021-07-27 12:23:23 +0200

    Create console app from template

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace silverflow_rebase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
